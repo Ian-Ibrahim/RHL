8b2a2a26431b4398243634cf5a39eb0dc9f899b3
Ian-Ibrahim <ian.okova@strathmore.edu>
2023-09-09 11:11:51 +0300

    Tawk to intergration

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/59dc57f1c28eca75e46251b3/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+// end  of tawk to
+function showSearchModal(){
+  searchModal.style.bottom='-9rem';
+  searchModal.classList.add('searh-modal-appeared');
+}
+function closeSearchModal(){
+  searchModal.style.bottom='14rem';
+}
+function navSearch(){
+  let queryTerm=document.getElementById('query').value
+  window.location.href=`search.html?keyword=${queryTerm}`;
+  return false;
+}
+function searchSubmit(){
+  return false;
+}
 function openCategoryPage(category) {
     window.location.href = `category.html?category=${category}`;
 }
